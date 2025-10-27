978ef167a562800da53e4b14b70de4ab0546ecca
Rajshri Patil <rajshripatil19022006@gmail.com>
2025-10-27 19:12:33 +0530

    login option added on register pg

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -39,15 +39,25 @@ function RegisterPage() {
         }
     }
     return(
+        <>
         <div>
             <h1 className='text-center font-mono text-5xl'>Register</h1>
             <form className="space-y-6"  onSubmit={handleSubmit}>
                 <input className='text-slate-100 text-sm font-medium mb-2 block' type="email" placeholder='Email'value={email} onChange={(e) => setEmail(e.target.value)}/>
                 <input className='text-slate-100 text-sm font-medium mb-2 block' type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
                 <input className='text-slate-100 text-sm font-medium mb-2 block' type="password" placeholder='Confirm Password'value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
-                <button className='w py-3 px-4 text-sm tracking-wider font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none cursor-pointer' type='submit'>Register</button>
+                <div className=''>
+                    <button className='flex py-3 px-4 text-sm tracking-wider font-medium rounded-md text-white bg-blue-600 hover:bg-blue-900 focus:outline-none cursor-pointer' type='submit'>Register</button>
+                </div>
             </form>
         </div>
+        
+        
+    
+        <div>
+            <p className='text-slate-400 text-sm mt-6 text-center'>Already have an account? <a href="/login" className='text-blue-600 font-medium hover:underline ml-1'>Login here</a></p>
+        </div>
+        </>
     )
 }
 
