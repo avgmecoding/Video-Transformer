3bd292bf67654b6d15e7c07a2a91cc62d31ee212
Rajshri Patil <rajshripatil19022006@gmail.com>
2025-10-28 07:29:41 +0530

    register page done

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -10,7 +10,7 @@ function RegisterPage() {
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if(password !== confirmPassword){
+        if (password !== confirmPassword) {
             alert("password did not matched");
             return;
         }
@@ -22,41 +22,38 @@ function RegisterPage() {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
-                    email, 
+                    email,
                     password,
                 })
             })
             const data = await response.json();
 
-            if(!response.ok){
+            if (!response.ok) {
                 throw new Error(data.error || "Registration Failed")
             }
             console.log(data);
             router.push("/login")
-            
+
         } catch (error) {
             console.log(error);
         }
     }
-    return(
+    return (
         <>
-        <div>
-            <h1 className='text-center font-mono text-5xl'>Register</h1>
-            <form className="space-y-6"  onSubmit={handleSubmit}>
-                <input className='text-slate-100 text-sm font-medium mb-2 block' type="email" placeholder='Email'value={email} onChange={(e) => setEmail(e.target.value)}/>
-                <input className='text-slate-100 text-sm font-medium mb-2 block' type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
-                <input className='text-slate-100 text-sm font-medium mb-2 block' type="password" placeholder='Confirm Password'value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
-                <div className=''>
-                    <button className='flex py-3 px-4 text-sm tracking-wider font-medium rounded-md text-white bg-blue-600 hover:bg-blue-900 focus:outline-none cursor-pointer' type='submit'>Register</button>
-                </div>
-            </form>
-        </div>
-        
-        
-    
-        <div>
-            <p className='text-slate-400 text-sm mt-6 text-center'>Already have an account? <a href="/login" className='text-blue-600 font-medium hover:underline ml-1'>Login here</a></p>
-        </div>
+            <div>
+                <h1 className='text-center font-mono text-5xl'>Register</h1>
+                <form className="space-y-6" onSubmit={handleSubmit}>
+                    <input className='text-slate-100 text-sm font-medium mb-2 block' type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input className='text-slate-100 text-sm font-medium mb-2 block' type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <input className='text-slate-100 text-sm font-medium mb-2 block' type="password" placeholder='Confirm Password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                    <div className=''>
+                        <button className='flex py-3 px-4 text-sm tracking-wider font-medium rounded-md text-white bg-blue-600 hover:bg-blue-900 focus:outline-none cursor-pointer' type='submit'>Register</button>
+                    </div>
+                </form>
+            </div>
+            <div>
+                <p className='text-slate-400 text-sm mt-6 text-center'>Already have an account? <a href="/login" className='text-blue-600 font-medium hover:underline ml-1'>Login here</a></p>
+            </div>
         </>
     )
 }
