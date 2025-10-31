afe9bc08e17d345e33fc893eb3418a2d070018af
Rajshri Patil <rajshripatil19022006@gmail.com>
2025-10-31 22:06:43 +0530

    final commit

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -1,5 +1,9 @@
+import { ImageKitProvider } from "@imagekit/next"
+import { SessionProvider } from "next-auth/react"
 
+const urlEndPoint = process.env.NEXT_PUBLIC_URL_ENDPOINT!;
 
-export default function Providers({childern} : {childern: React.ReactNode}){
-    return <>{childern}</>
+export default function Providers({children} : {children: React.ReactNode}){
+    return <SessionProvider refetchInterval={5*60}>
+        <ImageKitProvider urlEndpoint={urlEndPoint}>{children}</ImageKitProvider></SessionProvider>
 }
